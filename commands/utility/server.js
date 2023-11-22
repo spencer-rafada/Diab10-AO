c65ebcee1593449827636a06a4a1bca82f22c73e
spencer-rafada <spencer.byui@gmail.com>
2023-11-21 22:59:18 -0700

    basic structure of the bot

---
+++ commands/utility/server.js
@@ -0,0 +1,10 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('server')
+		.setDescription('Provides information about the server'),
+	async execute(interaction) {
+		await interaction.reply(`This server's name is: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}`);
+	},
+};
---
+++ events/interactionsCreate.js
@@ -0,0 +1,53 @@
+const { Events, Collection } = require('discord.js');
+
+module.exports = {
+	name: Events.InteractionCreate,
+	async execute(interaction) {
+		if (!interaction.isChatInputCommand()) return;
+
+		const command = interaction.client.commands.get(interaction.commandName);
+
+		// Sends error if command does not exist
+		if (!command) {
+			console.error(`[ERROR] The command ${interaction.commandName} does not exist.`);
+			return;
+		}
+
+		const { cooldowns } = interaction.client;
+
+		if (!cooldowns.has(command.data.name)) {
+			cooldowns.set(command.data.name, new Collection());
+		}
+
+		const now = Date.now();
+		const timestamps = cooldowns.get(command.data.name);
+		console.log(command);
+		const defaultCooldownDuration = 3;
+		const cooldownAmount = (command.cooldown ?? defaultCooldownDuration) * 1000;
+
+		if (timestamps.has(interaction.user.id)) {
+			const expirationTime = timestamps.get(interaction.user.id) + cooldownAmount;
+
+			if (now < expirationTime) {
+				const expiredTimestamp = Math.round(expirationTime / 1000);
+				return interaction.reply({ content: `Please wait, you are on a cooldown for \`${command.data.name}\`. You can use it again <t:${expiredTimestamp}:R>.`, ephemeral: true });
+			}
+		}
+
+		timestamps.set(interaction.user.id, now);
+		setTimeout(() => timestamps.delete(interaction.user.id), cooldownAmount);
+
+		try {
+			await command.execute(interaction);
+		}
+		catch (error) {
+			console.error(`[ERROR] ${error}`);
+			if (interaction.replied || interaction.deffered) {
+				await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
+			}
+			else {
+				await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+			}
+		}
+	},
+};
