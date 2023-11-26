693fd3e6b1df9aa13e263b9ab9c6355f2d358209
spencer-rafada <spencer.byui@gmail.com>
2023-11-25 22:45:41 -0700

    added pve and pvp

--- commands/hellgates/pvp.js
+++ commands/hellgates/pvp.js
@@ -2,15 +2,15 @@ const { SlashCommandBuilder } = require('discord.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
-		.setName('pve')
-		.setDescription('Add user to tally of people who did PvE today!')
+		.setName('pvp')
+		.setDescription('Add user to tally of people who did PvP today!')
 		.addUserOption(option =>
-			option.setName('user').setDescription('Sino ang nag PvE?').setRequired(true)),
+			option.setName('user').setDescription('Sino ang nag PvP?').setRequired(true)),
 	async execute(interaction) {
 		const user = interaction.options.getUser('user');
 		console.log(user);
 		// TODO: add user id and username to database and date of the command
 		// respond with confirmation of all the information said above
-		await interaction.reply(`${user.username} ay nag PvE ngayon!`);
+		await interaction.reply(`${user.username} ay nag PvP ngayon!`);
 	},
 };
