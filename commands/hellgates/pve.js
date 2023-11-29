bce92874fae22643fa7cb38e72d5f65819445549
spencer-rafada <spencer.byui@gmail.com>
2023-11-28 19:00:11 -0700

    added mongoose functionality

--- commands/hellgates/pve.js
+++ commands/hellgates/pve.js
@@ -1,4 +1,5 @@
 const { SlashCommandBuilder } = require('discord.js');
+const pveSchema = require('../../model/pveSchema.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -8,9 +9,33 @@ module.exports = {
 			option.setName('user').setDescription('Sino ang nag PvE?').setRequired(true)),
 	async execute(interaction) {
 		const user = interaction.options.getUser('user');
-		console.log(user);
-		// TODO: add user id and username to database and date of the command
-		// respond with confirmation of all the information said above
+		// console.log(user);
+
+		const existingUser = await pveSchema.findOne({ username: user.username, userId: user.id });
+
+		if (existingUser) {
+			existingUser.count += 1;
+			try {
+				await existingUser.save();
+				console.log(`[SUCCESS] Updated user: ${existingUser.username}`);
+			}
+			catch (err) {
+				console.error(`[ERROR] Failed to update user: ${err}`);
+			}
+		}
+		else {
+			try {
+				await new pveSchema({
+					username: user.username,
+					userId: user.id,
+					count: 1,
+				}).save();
+				console.log(`[SUCCESS] Saved user: ${user.username}`);
+			}
+			catch (err) {
+				console.error(`[ERROR] Failed to save user: ${err}`);
+			}
+		}
 		await interaction.reply(`${user.username} ay nag PvE ngayon!`);
 	},
 };
